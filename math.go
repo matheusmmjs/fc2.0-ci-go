1464deef8a2d1a989a1e31aaf6549bace662c643
matheusmmjs <matheusmatmac@hotmail.com>
2021-03-03 21:39:15 -0300

    feat: :sparkles: Add sonarqube

--- math.go
+++ math.go
@@ -9,3 +9,23 @@ func main() {
 func soma(a int, b int) int {
 	return a + b
 }
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func times(a int, b int) int {
+	return a * b
+}
+
+func somaX(a int, b int) int {
+	return a + b + a
+}
+
+func subX(a int, b int) int {
+	return a - b - a
+}
+
+func timesX(a int, b int) int {
+	return a * b * a
+}
